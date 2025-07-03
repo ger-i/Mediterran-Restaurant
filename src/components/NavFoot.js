bda734aad4be2f0b8183317be822aee04a9ae054
ger-i <138193532+ger-i@users.noreply.github.com>
2025-07-03 05:59:23 +0200

    Update NavFoot.js

--- src/components/NavFoot.js
+++ src/components/NavFoot.js
@@ -2,12 +2,18 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";  
 import { Outlet } from "react-router-dom";  
 
+/**
+ * NavFoot komponens - Layout wrapper
+ * Minden oldalon megjelenő közös elemek: navigációs sáv és lábléc
+ * A React Router Outlet segítségével a közép részbe kerül az aktuális oldal tartalma
+ */
+
 const NavFoot = () => { 
   return (
     <>  
-      <Navbar />  {/* A Navbar komponensünk a weboldalunk fejlécét tartalmazza. */}
-      <Outlet />  {/* Az Outlet komponens a beágyazott aloldalakat jeleníti meg. */}
-      <Footer />  {/* A Footer komponensünk a weboldalunk láblécét tartalmazza. */}
+      <Navbar />  {/* Felső navigációs sáv - minden oldalon megjelenik */}
+      <Outlet />  {/* Outlet - ide kerül az aktuális route tartalma. Ez a hely dinamikusan változik az URL szerint */}
+      <Footer />  {/* Alsó lábléc - minden oldalon megjelenik */}
     </>
   );
 };
