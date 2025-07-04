27284d70ff0b1dd30b3b30cf9e803126879c159d
ger-i <138193532+ger-i@users.noreply.github.com>
2025-07-04 05:31:34 +0200

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,38 @@
 import "./Card.css";
 
+/**
+ * Card komponens - Étlap elem megjelenítése
+ * Újrafelhasználható komponens egyedi étel/ital bemutatására
+ * Prop-ként kapja meg az étel adatait és strukturáltan jeleníti meg
+ */
+
 const Card = ({ data }) => {
   return (
+    // Kártya fő konténere
     <div className="card">
-      <div className="imgBx" style={{ background: `url(${data.image}) no-repeat center / cover` }}></div>
+      {/* Kép szekció - inline stílus használata dinamikus háttérképhez */}
+      <div 
+        className="imgBx" 
+        style={{ 
+          background: `url(${data.image}) no-repeat center / cover` 
+        }}
+      >
+      </div>
+      
+      {/* Tartalmi szekció - szöveg alapú információk */}
       <div className="content">
+        {/* Ár megjelenítése */}
         <span className="price">
+          {/* Ár formázása forint valutával */}
           <span>{`${data.price} Ft`}</span>
         </span>
+        
+        {/* Termék részletek szekció */}
         <div className="product-details">
+          {/* Étel neve */}
           <h1>{data.name}</h1>
+          
+          {/* Étel leírása - fallback szöveggel hiányzó leírás esetén */}
           <p>{data.description || "Nem áll rendelkezésre leírás."}</p>
         </div>
       </div>
