e8b022215e39699856a9bf4caf84121927c2be69
ger-i <geri3dvfx@gmail.com>
2025-08-11 17:08:36 +0200

    Update components

--- src/components/Special.js
+++ src/components/Special.js
@@ -1,30 +1,28 @@
-import Card from "./Card";  
-import "./Special.css";
+import Card from "./Card";
 
 /**
- * Special komponens - Heti akciók szekció
+ * Special komponens - Heti akciók szekció Tailwind CSS-sel
  * Megjeleníti a heti akcióban lévő ételeket Card komponensek segítségével
  * Dinamikusan rendereli a prop-ként kapott adatokat
  */
 
-const Special = ({ data }) => { 
+const Special = ({ data }) => {
   return (
-    // Heti akciók szekció - ID-val ellátva navigációs célokra
-    <section id="special">  
-      <h2>Heti akcióink!</h2>
-      
-      {/* Akciós ételek konténere */}
-      <div className="special-body">  
+    // Heti akciók szekció - ID-val ellátva navigációhoz
+    <section id="special" className="py-8">
+      {/* Cím - középre igazítva */}
+      <h2 className="text-center text-xl md:text-2xl font-semibold mb-6">
+        Heti akcióink!
+      </h2>
+      {/* Akciós ételek konténere - flexbox, wrap, középre igazítás, gap */}
+      <div className="flex flex-wrap justify-center gap-5">
         {/* Dinamikus renderelés - végigiterálás az adatok tömbjén */}
-        {data.map((element) => {  
-          return (
-            // Card komponens minden étel elemhez
-            <Card 
-              key={element.id} // key prop: React optimalizálás miatt szükséges egyedi azonosító
-              data={element} // data prop: étel adatainak átadása a Card komponensnek
-            />
-          );  
-        })}
+        {data.map((element) => (
+          <Card
+            key={element.id} // Egyedi kulcs az optimalizáláshoz
+            data={element}   // Adatok átadása a Card komponensnek
+          />
+        ))}
       </div>
     </section>
   );
